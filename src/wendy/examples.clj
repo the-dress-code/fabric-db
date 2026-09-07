7435b9f4d7eb4ac43817f3ba14b2ee9c3416ff95
Wendy Randolph <wendy.randolph5@gmail.com>
2026-09-07 18:31:08 -0400

    Add comments where some vars will not resolve and cause exceptions

--- src/wendy/examples.clj
+++ src/wendy/examples.clj
@@ -1,10 +1,11 @@
 (ns wendy.examples
   (:gen-class)
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [wendy.fabric-db :as fabric-db]))
 
 (d/q '[:find ?e
        :where [?e :fabric/pattern :pattern/solid]]
-      (d/db conn))
+      (d/db wendy.fabric-db/conn))
 
 (def all-blue-plant-fabrics
   "A query to return all the blue and plant fabric entity ids"
@@ -12,6 +13,7 @@
     :where [?e :fabric/color :color/blue]
            [?e :fabric/fiber-origin :fiber-origin/plant]])
 
+;; db is not defined here so will error
 (def blue-plant-eids (d/q all-blue-plant-fabrics db))
 
 (def color-intensity-of-blue-plant-fabric
@@ -21,12 +23,16 @@
            [?e :fabric/fiber-origin :fiber-origin/plant]
            [?e :fabric/color-intensity ?color-intensity]])
 
+;; db is not defined here so will error
 (d/q color-intensity-of-blue-plant-fabric db)
 
+;; db is not defined here so will error
 (d/entity db 17592186045464)
 
+;; db is not defined here so will error
 (d/pull db '[*] 17592186045464)
 
+;; db is not defined here so will error
 (d/touch (d/entity db 17592186045464))
 
 wendy.fabric-db/all-attrib-values-light-blue-cotton
